039b79b5a69b89489966d90cda6d8def0a0e460c
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-05-17 11:11:08 +0100

    data object
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

--- FTChartObject.h
+++ FTChartObject.h
@@ -1,15 +1,13 @@
 //
-//  FTBarChart.h
+//  FTChartObject.h
 //  TurkcellNavigation
 //
 //  Created by Ondrej Rafaj on 17/05/2013.
 //  Copyright (c) 2013 RGA. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "FTChartObject.h"
+#import <Foundation/Foundation.h>
 
-
-@interface FTBarChart : UIView
+@interface FTChartObject : NSObject
 
 @end
--- FTChartObject.m
+++ FTChartObject.m
@@ -1,15 +1,13 @@
 //
-//  FTBarChart.h
+//  FTChartObject.m
 //  TurkcellNavigation
 //
 //  Created by Ondrej Rafaj on 17/05/2013.
 //  Copyright (c) 2013 RGA. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "FTChartObject.h"
 
-
-@interface FTBarChart : UIView
+@implementation FTChartObject
 
 @end

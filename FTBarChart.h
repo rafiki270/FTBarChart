b600af4bff8769d360f7b9ed168e06bb3a4c4869
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-05-17 09:35:54 +0100

    Initial upload
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

---
+++ FTBarChart.h
@@ -0,0 +1,13 @@
+//
+//  FTBarChart.h
+//  TurkcellNavigation
+//
+//  Created by Ondrej Rafaj on 17/05/2013.
+//  Copyright (c) 2013 RGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTBarChart : UIView
+
+@end
---
+++ FTBarChart.m
@@ -0,0 +1,31 @@
+//
+//  FTBarChart.m
+//  TurkcellNavigation
+//
+//  Created by Ondrej Rafaj on 17/05/2013.
+//  Copyright (c) 2013 RGA. All rights reserved.
+//
+
+#import "FTBarChart.h"
+
+@implementation FTBarChart
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
